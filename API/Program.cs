d2d1832423b0d868f6d7dd5cfece82b30e1f8053
Dusan <dusan.jovanovicno.4@gmail.com>
2022-09-15 11:10:31 +0200

    add error handling middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -33,6 +34,7 @@ catch (Exception ex)
 
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
