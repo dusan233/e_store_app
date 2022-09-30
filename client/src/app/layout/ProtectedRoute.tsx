187a6bc0036080909dcd52cfc3cf0f5641c3c5c0
Dusan <dusan.jovanovicno.4@gmail.com>
2022-09-30 23:16:30 +0200

    add protected route component

---
+++ client/src/app/layout/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+// import {  } from "react-router-dom";
+// import { Route, RouteProps } from "react-router-dom";
+// import { useAppSelector } from "../store/configureStore";
+
+// interface Props extends RouteProps {
+//   component: any;
+// }
+// const ProtectedRoute = ({ component: Component, ...ress }: Props) => {
+//   let { user } = useAppSelector((state) => state.account);
+//   return (
+//     <Route
+//       {...rest}
+//       render={({ location }) => (user ? <Component /> : <Redirect />)}
+//     />
+//   );
+// };
+
+// export default ProtectedRoute;
