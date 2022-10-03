2ea0b1d8125e360906f911752b531e778265027b
Dusan <dusan.jovanovicno.4@gmail.com>
2022-10-03 10:14:31 +0200

    add Order entities

--- client/src/app/layout/ProtectedRoute.tsx
+++ client/src/app/layout/ProtectedRoute.tsx
@@ -1,18 +1,13 @@
-// import {  } from "react-router-dom";
-// import { Route, RouteProps } from "react-router-dom";
-// import { useAppSelector } from "../store/configureStore";
+import {} from "react-router-dom";
+import { Route, RouteProps } from "react-router-dom";
+import { useAppSelector } from "../store/configureStore";
 
-// interface Props extends RouteProps {
-//   component: any;
-// }
-// const ProtectedRoute = ({ component: Component, ...ress }: Props) => {
-//   let { user } = useAppSelector((state) => state.account);
-//   return (
-//     <Route
-//       {...rest}
-//       render={({ location }) => (user ? <Component /> : <Redirect />)}
-//     />
-//   );
-// };
+interface Props extends RouteProps {
+  component: any;
+}
+const ProtectedRoute = ({ component: Component, ...ress }: Props) => {
+  let { user } = useAppSelector((state) => state.account);
+  return <div>ds</div>;
+};
 
-// export default ProtectedRoute;
+export default ProtectedRoute;
