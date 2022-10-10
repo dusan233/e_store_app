af4952a8088054192bf9c63f54cf5300a8ebcba6
Dusan <dusan.jovanovicno.4@gmail.com>
2022-10-10 16:16:56 +0200

    configure mui theme

---
+++ client/src/app/layout/themeOptions.ts
@@ -0,0 +1,74 @@
+import { ThemeOptions } from "@mui/material";
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    background: {
+      default: "#f5f4f2",
+    },
+    primary: {
+      main: "#FDC90E",
+    },
+    secondary: {
+      main: "#f54343",
+    },
+  },
+  components: {
+    MuiTooltip: {
+      defaultProps: {
+        arrow: true,
+      },
+    },
+
+    MuiAppBar: {
+      defaultProps: {
+        color: "inherit",
+      },
+      styleOverrides: {
+        colorInherit: {
+          backgroundColor: "#181717",
+          color: "#fff",
+        },
+      },
+    },
+    MuiSwitch: {
+      styleOverrides: {
+        root: {
+          width: 42,
+          height: 26,
+          padding: 0,
+          margin: 8,
+        },
+        switchBase: {
+          padding: 1,
+          "&$checked, &$colorPrimary$checked, &$colorSecondary$checked": {
+            transform: "translateX(16px)",
+            color: "#fff",
+            "& + $track": {
+              opacity: 1,
+              border: "none",
+            },
+          },
+        },
+        thumb: {
+          width: 24,
+          height: 24,
+        },
+        track: {
+          borderRadius: 13,
+          border: "1px solid #bdbdbd",
+          backgroundColor: "#fafafa",
+          opacity: 1,
+          transition:
+            "background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+        },
+      },
+    },
+  },
+  spacing: 8,
+  shape: {
+    borderRadius: 0,
+  },
+};
+
+export default themeOptions;
